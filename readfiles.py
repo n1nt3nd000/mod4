1408a18a534a6a9a371ede063e31ad72a80bbd8b
n1nt3nd000 <baraberea2345@gmail.com>
2024-06-10 00:02:59 +0300

    Update readfiles.py

--- readfiles.py
+++ readfiles.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
 import boto3
 import pandas as pd
 from io import StringIO
@@ -13,19 +19,34 @@ csv_file_key = 'currency_rate_2022.csv'
 def read_csv_and_compute_monthly_average_from_s3(bucket_name, file_key):
     obj = s3.get_object(Bucket=bucket_name, Key=file_key)
     data = obj['Body'].read().decode('utf-8')
-    df = pd.read_csv(StringIO(data))
+    df = pd.read_csv('currency_rate_2022.csv', usecols=['date', 'rate', 'cc'])
     
     # Перетворюємо стовпець Date в тип datetime
     df['date'] = pd.to_datetime(df['date'])
     
-    # Групуєємо за місяцями та обчислюємо середній курс
-    df['Month'] = df['date'].dt.to_period('M')
-    monthly_average = df.groupby('Month')['rate'].mean().reset_index()
+    # Розділення даних за тип валюти (USD та EUR)
+    usd_df = df[df['cc'] == 'USD']
+    eur_df = df[df['cc'] == 'EUR']
     
-    return monthly_average
+    # Групуєємо за місяцями та обчислюємо середній курс
+    usd_df['Month'] = usd_df['date'].dt.to_period('M')
+    eur_df['Month'] = eur_df['date'].dt.to_period('M')
+    
+    monthly_average_usd = usd_df.groupby('Month')['rate'].mean().reset_index()
+    monthly_average_eur = eur_df.groupby('Month')['rate'].mean().reset_index()
+    
+    return monthly_average_usd, monthly_average_eur
 
 # Зчитування та обчислення середнього курсу за місяць з CSV файлу на S3
-monthly_average_df = read_csv_and_compute_monthly_average_from_s3(bucket_name, csv_file_key)
+monthly_average_usd_df, monthly_average_eur_df = read_csv_and_compute_monthly_average_from_s3(bucket_name, csv_file_key)
 
 # Виведення вмісту DataFrame на консоль
-print(monthly_average_df)
+print("Monthly average USD:\n", monthly_average_usd_df)
+print("\nMonthly average EUR:\n", monthly_average_eur_df)
+
+
+# In[ ]:
+
+
+
+
